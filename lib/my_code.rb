876b9071a9b62da8738cefd866ed6b1265ca1b3d
Andrew Thiessen <squaids449@gmail.com>
2019-08-15 23:24:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -20,13 +20,7 @@ def reduce_to_total(source_array, starting_point=0)
   return temp
 end
 def reduce_to_all_true(source_array)
-  i=0
-  while i < source_array.length do
-    i++
-    if source_array[i] == true
-      return true
-  end
-  return false
+  
 end
 def reduce_to_any_true(source_array)
   
