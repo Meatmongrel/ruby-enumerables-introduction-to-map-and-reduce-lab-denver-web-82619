24c34156fac420c9e0ffad4488aff2fa1155f51e
Andrew Thiessen <squaids449@gmail.com>
2019-08-15 23:23:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,9 +23,10 @@ def reduce_to_all_true(source_array)
   i=0
   while i < source_array.length do
     i++
-    
+    if source_array[i] == true
+      return true
   end
-
+  return false
 end
 def reduce_to_any_true(source_array)
   
