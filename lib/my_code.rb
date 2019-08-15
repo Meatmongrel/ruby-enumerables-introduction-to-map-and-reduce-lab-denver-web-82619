93c8b8a4121083a7a330a6bd5ad06c5a071e759a
Andrew Thiessen <squaids449@gmail.com>
2019-08-15 23:10:29 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -15,7 +15,7 @@ def map_to_square(source_array)
   temp = source_array.map{ |x| x ** 2}
   return temp
 end
-def reduce_to_total(source_array, starting_point=nil)
+def reduce_to_total(source_array, starting_point=0)
   temp = source_array.reduce(starting_point){ |sum, x| sum += x }
   return temp
 end
